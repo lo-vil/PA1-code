6535ad0841380df3ff9af9f875341b6d9fff3957
lo-vil <louis.villemure@gmail.com>
2023-03-12 14:14:50 -0400

    first commit

---
+++ Driver.java
@@ -0,0 +1,29 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kerly Titus
+ */
+public class Driver {
+
+    /** 
+     * main class
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    	Network objNetwork = new Network("network");            /* Activate the network */
+        objNetwork.start();
+        Server objServer = new Server();        
+        objServer.start();
+        Client objClientSender = new Client("sending");
+        Client objClientReceiver = new Client("receiving");
+        objClientSender.start();
+        objClientReceiver.start();
+    }
+}
